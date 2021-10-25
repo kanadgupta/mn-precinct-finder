cbb29fe96450cabbf0fa0b159a1d18d17d9c607c
Kanad Gupta <kgupta@umn.edu>
2021-10-25 15:58:13 -0500

    test: add test coverage

---
+++ __test__/build-maps-url.test.js
@@ -0,0 +1,13 @@
+const buildMapsUrl = require('../lib/build-maps-url');
+
+describe('#buildMapsUrl', () => {
+  it('should return URL with place ID if it is passed', () => {
+    expect(buildMapsUrl('123 Main St', 'test_id')).toBe(
+      'https://www.google.com/maps/search/?api=1&query=123+Main+St&query_place_id=test_id'
+    );
+  });
+
+  it('should return URL without place ID if none is passed', () => {
+    expect(buildMapsUrl('123 Main St')).toBe('https://www.google.com/maps/search/?api=1&query=123+Main+St');
+  });
+});
