428e14ba088548b89ccfdb60a687b6934e48971f
Kanad Gupta <kgupta@umn.edu>
2021-10-24 19:47:10 -0500

    chore: add eslint support for es2020

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   extends: ['@readme/eslint-config', '@readme/eslint-config/testing'],
   ignorePatterns: ['coverage/', 'lib/postprocess.js'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
 };
