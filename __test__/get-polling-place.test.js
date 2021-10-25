cae9640377fb0abd29d2ac3e63a1cbffb720843b
Kanad Gupta <kgupta@umn.edu>
2021-10-25 14:45:16 -0500

    feat: add getter for polling place, plus test suite

---
+++ __test__/get-polling-place.test.js
@@ -0,0 +1,25 @@
+const getPollingPlace = require('../lib/get-polling-place');
+const pollingPlacesData = require('../lib/data/mpls-2021-polling-places.json');
+const precinctData = require('../lib/data/mn-precincts.json');
+
+describe('#getPollingPlace', () => {
+  it('should retrieve polling place data for valid precinct name', () => {
+    expect(getPollingPlace('Minneapolis W-1 P-3')).toStrictEqual({
+      address: '2955 Hayes St NE',
+      building: 'Northeast Middle School',
+      directions: 'enter via side/rear off 29th Ave NE',
+    });
+  });
+
+  it('should return fallback for invalid precinct name', () => {
+    expect(getPollingPlace('Minneapolis W-1000')).toStrictEqual({});
+  });
+});
+
+describe('polling place data', () => {
+  const precinctKeys = Object.keys(pollingPlacesData);
+
+  it.each(precinctKeys)('precinct %s exists in geojson data', key => {
+    expect(precinctData.features.some(feature => feature.properties.Precinct === key)).toBe(true);
+  });
+});
