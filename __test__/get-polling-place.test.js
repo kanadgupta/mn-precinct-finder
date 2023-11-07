f180d16bb4d908a1f33b7213c5edf92c5a23f1fa
Kanad Gupta <git@kanad.dev>
2023-11-07 17:03:33 -0600

    test: get tests working with updated data

--- __test__/get-polling-place.test.js
+++ __test__/get-polling-place.test.js
@@ -13,6 +13,16 @@ describe('#getPollingPlace', () => {
     });
   });
 
+  it('should retrieve polling place data for valid precinct name with zeros', () => {
+    expect(getPollingPlace('Minneapolis W-01 P-03')).toStrictEqual({
+      address: '2955 Hayes St NE',
+      building: 'Northeast Middle School',
+      directions: 'enter via side/rear off 29th Ave NE',
+      gmapsUrl:
+        'https://www.google.com/maps/search/?api=1&query=Northeast+Middle+School+2955+Hayes+St+NE&query_place_id=ChIJf6uXUNAts1IRx9gO5VPsm0M',
+    });
+  });
+
   it('should return fallback for invalid precinct name', () => {
     expect(getPollingPlace('Minneapolis W-1000')).toStrictEqual({});
   });
@@ -22,6 +32,12 @@ describe('polling place data', () => {
   const precinctKeys = Object.keys(pollingPlacesData);
 
   it.each(precinctKeys)('precinct %s exists in geojson data', key => {
-    expect(precinctData.features.some(feature => feature.properties.Precinct === key)).toBe(true);
+    expect(
+      precinctData.features.some(
+        feature =>
+          feature.properties.Precinct === key ||
+          feature.properties.Precinct.replace(/W-0/g, 'W-').replace(/P-0/g, 'P-') === key
+      )
+    ).toBe(true);
   });
 });
