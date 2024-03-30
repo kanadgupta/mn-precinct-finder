2d83dccc09b87863cee03258e71c87be315fb1dd
Kanad Gupta <git@kanad.dev>
2024-03-30 15:47:36 -0500

    ci: cleanup

---
+++ bin/build-openapi.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const app = require('../server');
+
+const outfile = 'openapi.json';
+
+async function run() {
+  try {
+    await app.ready();
+    const spec = JSON.stringify(app.swagger());
+    fs.writeFileSync(outfile, spec, { encoding: 'utf-8' });
+    // eslint-disable-next-line no-console
+    console.log('successfully wrote file to', outfile);
+    process.exit(0);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('Error syncing file!');
+    // eslint-disable-next-line no-console
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+run();
