0a16da2381a747385f4ae4289bb5ee50cfcc7c0a
Kanad Gupta <git@kanad.dev>
2025-01-11 18:44:04 -0600

    chore: rename eslint files

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@readme/eslint-config'],
-  root: true,
-  ignorePatterns: ['coverage/', 'lib/postprocess.js'],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-};
--- __test__/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['@readme/eslint-config', '@readme/eslint-config/testing/vitest'],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-};
