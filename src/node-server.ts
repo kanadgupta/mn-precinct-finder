8b5b4786de7be14065f3a077c52d8f8bfb78f306
Kanad Gupta <git@kanad.dev>
2025-04-08 12:36:46 -0500

    chore: add support for PORT env var

--- src/node-server.ts
+++ src/node-server.ts
@@ -5,7 +5,8 @@ import app from './server.js';
 
 app.use('/style.css', serveStatic({ path: './public/style.css' }));
 
-serve({ fetch: app.fetch, port: 3000 }, info => {
+// port 3000 is what we specify for fly.io (see Dockerfile and fly.toml)
+serve({ fetch: app.fetch, port: Number(process.env.PORT) || 3000 }, info => {
   // eslint-disable-next-line no-console
   console.log(`Listening on http://localhost:${info.port}`);
 });
