f8d6f9ae94f72c8e835732a14204d46f788edc0b
Kanad Gupta <git@kanad.dev>
2025-04-08 12:10:42 -0500

    fix: port

--- src/node-server.ts
+++ src/node-server.ts
@@ -5,7 +5,7 @@ import app from './server.js';
 
 app.use('/style.css', serveStatic({ path: './public/style.css' }));
 
-serve({ fetch: app.fetch, port: 1234 }, info => {
+serve({ fetch: app.fetch, port: 3000 }, info => {
   // eslint-disable-next-line no-console
   console.log(`Listening on http://localhost:${info.port}`);
 });
