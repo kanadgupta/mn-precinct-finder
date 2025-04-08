f13cefc4e953c589d41b1048b598903186cc59cb
Kanad Gupta <git@kanad.dev>
2025-04-08 11:49:39 -0500

    chore: uninstall @flydotio/dockerfile

--- knip.ts
+++ knip.ts
@@ -2,7 +2,6 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   entry: ['bin/postprocess.js'],
-  ignoreDependencies: ['@flydotio/dockerfile'],
 };
 
 export default config;
